339e6c42add3c59fe70869dbd10a4865d4b451de
j-j-k <jjkim1265@gmail.com>
2018-02-16 21:05:33 -0500

    DriveForADistance

--- src/org/usfirst/frc/team614/robot/OI.java
+++ src/org/usfirst/frc/team614/robot/OI.java
@@ -7,7 +7,6 @@ import org.team708.robot.util.Gamepad;
 import org.usfirst.frc.team614.robot.commands.autonomous.DeliverFromLeft;
 import org.usfirst.frc.team614.robot.commands.autonomous.DeliverFromRight;
 import org.usfirst.frc.team614.robot.commands.drivetrain.DriveForADistance;
-import org.usfirst.frc.team614.robot.commands.drivetrain.DriveStraight;
 import org.usfirst.frc.team614.robot.commands.drivetrain.RotateToAngle;
 import org.usfirst.frc.team614.robot.commands.intake.RevIntake;
 import org.usfirst.frc.team614.robot.commands.intake.RevOutake;
@@ -62,9 +61,6 @@ public class OI {
 	
 	private static final Button deliverScale = new JoystickButton(operatorGamepad, Gamepad.button_L_Shoulder);
 	private static final Button deliverSwitch = new JoystickButton(operatorGamepad, Gamepad.button_R_Shoulder);
-	
-	private static final Button deliverScales = new JoystickButton(driverGamepad, Gamepad.button_L_Shoulder);
-	private static final Button deliverSwitchs = new JoystickButton(driverGamepad, Gamepad.button_R_Shoulder);
 
 	public OI() {
 		revIntake.whileHeld(new RevIntake());
@@ -76,7 +72,7 @@ public class OI {
 		deliverScale.whileHeld(new DeliverScale());
 		deliverSwitch.whileHeld(new DeliverSwitch());
 		
-		deliverScales.whenPressed(new DriveForADistance(22.3, 0.5));
-		deliverSwitchs.whenPressed(new DriveStraight(0.5));
+		deliverScale.whileHeld(new DriveForADistance(40, 0.5));
+		deliverSwitch.whileHeld(new RotateToAngle(90, false));
 	}
 }
